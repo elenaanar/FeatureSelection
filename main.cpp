ee19b0de9cd0199d97c00d2e134f1d3f9cd0f4bf
arrdyas <arrdyas@gmail.com>
2024-05-17 12:20:40 -0700

    fixed up main.cpp -- NOT DRIVER YET

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+/*
+    Authors: Elena Hosseinmardi: ehoss003
+            Arrdya Srivastav: asriv033
+            Anthony Padilla: apadi089
+            Luis Magallanes Banuelos: lmaga024
+*/
 #include <iostream> 
 #include "src/Search.h"
 #include <vector>
@@ -7,8 +13,27 @@ int main(){
 
     int features = 10; 
     Search search; 
-    vector<int> vec;
-    vec = search.greedyForward(features);
+    unordered_map<int, bool> subset = search.greedyBackward(features);
+    unordered_map<int, bool> subset2 = search.greedyForward(features);
+
+    for(auto it = subset.begin(); it != subset.end(); ++it){
+        cout << it->first << " "; 
+    }
+
+    if(subset.size() == 0){
+        cout << "No features selected." << endl;
+    }
+
+    cout << endl;
+
+    for(auto it = subset2.begin(); it != subset2.end(); ++it){
+        cout << it->first << " "; 
+    }
+
+    if(subset2.size() == 0){
+        cout << "No features selected." << endl;
+    }
+
 
     // for (unsigned i = 0; i < vec.size(); ++i){
     //     cout << vec[i] << " "; 
