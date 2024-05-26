4b1e42399d3cd869dcd0fc8fc72b53b8d890431d
Luis Magallanes <97562172+Luiy15@users.noreply.github.com>
2024-05-26 16:52:55 -0700

    Update Validator.cpp

--- src/Validator.cpp
+++ src/Validator.cpp
@@ -2,33 +2,39 @@
 
 // Function to validate the classifier
 double Validator::validate(vector<int> featureSubset, string dataLoc) {
-
+    // cout << dataLoc << endl;
   Classifier classifier(featureSubset);
   classifier.train(dataLoc);
 
-  int DataSize = classifier.getNumDataPoints();
+  double DataSize = classifier.getNumDataPoints();
 
   // Initialize variables for accuracy calculation
-  int correctPredictions = 0;
+  double correctPredictions = 0;
 
   // Leave-One-Out
   for (int id = 0; id < DataSize; id++) {
     // Evaluate the classifier for each data point
     correctPredictions += evaluate(id, classifier);
+    // cout << correctPredictions << endl;
   }
 
   // Calculate and return the accuracy
-  return static_cast<double>(correctPredictions) / DataSize;
+  return correctPredictions / DataSize;
 }
 
 int Validator::evaluate(int id, Classifier &classifier) {
   // Get the true class label from the data
-  const auto &data = classifier.getNumDataPoints();
+    int ClassLabelValue = 0; 
+  const auto &data = classifier.getData();
   int trueClassLabel = static_cast<int>(data[id][0]);
+    // cout << trueClassLabel << endl;
 
   // Test the classifier with the given ID
   int predictedClassLabel = classifier.test(id);
-
+    // cout << predictedClassLabel << endl;
+    if (predictedClassLabel == trueClassLabel){
+        ClassLabelValue = 1;
+    }
   // Return 1 if the classifier is correct, 0 if the classifier is incorrect
-  return (predictedClassLabel == trueClassLabel) ? 1 : 0; // shoutout to 120b
+  return ClassLabelValue; // shoutout to 120b
 }
