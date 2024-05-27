b65fcfe0f32ef920256bc379bcf71685cd9c7cde
Luis Magallanes <97562172+Luiy15@users.noreply.github.com>
2024-05-26 17:12:20 -0700

    updated Classifier.h

---
+++ src/Classifier.h
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+#ifndef CLASSIFIER_H
+#define CLASSIFIER_H
+
+class Classifier
+{
+private:
+    vector<vector<double>> data; // 2D vector to store data, index is the id and the first point in the sub array is the class.
+    vector<vector<double>> normalizingData;
+    vector<int> featureSubset;
+    int numDataPoints;
+    int numFeatures;
+
+public:
+    Classifier(vector<int> featureSubsetInput);
+    void train(string dataLoc);
+    int test(int id);
+    int test(vector<double> &features);
+    void print();
+    int getNumFeatures();
+    int getNumDataPoints();
+
+private:
+    void normalize(int col);
+    void normalizeFeatures(vector<double> &features);
+    int nn(vector<double> &, int excludeRow = -1);
+    int nn(int);
+    double distance(const vector<double> &, int id2) const;
+};
+
+#endif
