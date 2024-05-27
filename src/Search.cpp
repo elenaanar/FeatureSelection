d49808af5b467a89a1b78f9bc31087e6ccac5231
Luis Magallanes <97562172+Luiy15@users.noreply.github.com>
2024-05-26 17:26:20 -0700

    Update Search.cpp

--- src/Search.cpp
+++ src/Search.cpp
@@ -1,194 +1,127 @@
-#include "Classifier.h"
-#include <cmath>
+#include "Search.h"
 
-// Constructor
-Classifier::Classifier(vector<int> featureSubsetInput) {
-  featureSubset = featureSubsetInput;
+double Search::evaluate(const unordered_map<int, bool> &subset)
+{
+  return static_cast<double>(rand()) / RAND_MAX;
 }
 
-// Train the classifier
-void Classifier::train(string dataLoc) {
-  // Read in the data file
-  ifstream inputFile(dataLoc);
-  if (!inputFile.is_open()) {
-    std::cerr << "Failed to open data file." << std::endl;
-    return;
-  }
-
-  // Read data from file and store in vector
-  string mystring;
-  std::vector<double> featureVector;
-  // Get lines from the file, turn to double and then add to the feature vector
-  while (getline(inputFile, mystring)) {
-    istringstream ss(mystring);
-    double value;
-    while (ss >> value) {
-      featureVector.push_back(value);
+void printFeatures(const unordered_map<int, bool> &subset, double accuracy)
+{
+  cout << "Using features {";
+  auto it = subset.begin();
+  for (size_t i = 0; it != subset.end(); ++it, ++i)
+  {
+    if (i < subset.size() - 1)
+    {
+      cout << it->first << " ";
     }
-    data.push_back(featureVector);
-    featureVector.clear();
+    else
+    {
+      cout << it->first;
+    }
   }
-  // Get the number of features
-  numFeatures = data[0].size() - 1;
-  inputFile.close();
-  // print();
-  // normalize the data to fit between 0 and 1 for all features
-  // starting at 1 so that we dont normalize the class
-  for (int i = 1; i <= numFeatures; i++) {
-    normalize(i);
-  }
-
-  // printing the data vector
-  print();
+  cout << "} accuracy is: " << accuracy * 100 << "%" << endl;
 }
 
-// Predict the class label for a given feature vector
-// This is for a user to use to predict label for new data
-int Classifier::test(vector<double> &features) {
-  // Normalize the features
-  normalizeFeatures(features);
-
-  // Find the nearest neighbor
-
-  // Return the class label of the nearest neighbor
-  return nn(features);
-}
-
-// Predict the class label for a given id
-// This is for the validator to use to predict label for the test data
-int Classifier::test(int id) {
-  // No need to normalize the features since they are already normalized
-
-  // Find the nearest neighbor
-  // Remember to skip the id itself
-
-  // Return the class label of the nearest neighbor
-  return 0;
-}
-
-//---------------------------------------
-//           PRIVATE HELPERS
-//---------------------------------------
-
-// Calculate the Euclidean distance between two data points
-double Classifier::distance(const vector<double> &features, int row) const {
-  // make sure row is in range
-  if (row < 0 || row >= data.size()) {
-    throw out_of_range("id2 is out of bounds");
+unordered_map<int, bool> Search::greedyBackward(int totalFeatures)
+{
+  cout << "--------------------------" << endl; // added for clarity
+  cout << "Greedy Backward Search:" << endl;
+  cout << "--------------------------" << endl; // added for clarity
+  srand(time(NULL));
+  unordered_map<int, bool> currSubset;
+  for (unsigned i = 0; i < totalFeatures; ++i)
+  {
+    currSubset[i] = true;
   }
+  unordered_map<int, bool> globalBestSubset = currSubset;
+  double globalBestAcc = evaluate(currSubset);
+  printFeatures(currSubset, globalBestAcc);
+  cout << endl;
 
-  const vector<double> &datapoint = data[row];
-
-  // make sure feat vec is in range
-  for (int i : featureSubset) {
-    if (i < 1 || i >= datapoint.size()) {
-      throw out_of_range("featureSubset index is out of bounds");
+  for (unsigned i = 0; i < totalFeatures; ++i)
+  {
+    double currBestAcc = 0.0;
+    int worstFeature = -1;
+    for (const auto &[feature, value] : currSubset)
+    {
+      unordered_map<int, bool> tempset = currSubset;
+      tempset.erase(feature); // explore next feature
+      double a = evaluate(tempset);
+      printFeatures(tempset, a);
+      if (a > currBestAcc)
+      { // if feature yields less accuracy than last, set as worst feat
+        currBestAcc = a;
+        worstFeature = feature;
+      }
+    }
+    if (worstFeature != -1)
+    { // if we found a worst feature to delete
+      currSubset.erase(worstFeature);
+      if (currBestAcc >= globalBestAcc)
+      { // check if this is an overall better subset
+        globalBestAcc = currBestAcc;
+        globalBestSubset = currSubset;
+        bestAccuracy = globalBestAcc;
+      }
+      cout << endl
+           << "Best subset this round: ";
+      printFeatures(currSubset, currBestAcc);
+      cout << endl;
     }
   }
 
-  double sum = 0.0;
-  // for each feature that we're caluclating based on
-  for (int i = 0; i < featureSubset.size(); i++) {
-    int featindex = featureSubset[i]; // current feature we're comparing
-    if (featindex - (datapoint.size() - features.size()) < 0 ||
-        featindex - (datapoint.size() - features.size()) >= features.size()) {
-      throw out_of_range("Index adjustment out of bounds");
-    } // if that feat is out of range
-
-    // calculate diff between feature value
-    double diff = features[featindex - (datapoint.size() - features.size())] -
-                  datapoint[featindex];
-    sum += diff * diff;
-  }
-
-  return sqrt(sum);
+  // printFeatures(globalBestSubset, globalBestAcc);
+  return globalBestSubset;
 }
 
-// Normalize the data
-void Classifier::normalize(int col) {
-  // Find the minimum and maximum values in the specified column
-  double minVal = data[0][col];
-  double maxVal = data[0][col];
-  for (const auto &featureVector : data) {
-    double value = featureVector[col];
-    if (value < minVal) {
-      minVal = value;
-    }
-    if (value > maxVal) {
-      maxVal = value;
-    }
-  }
-  vector<double> temp;
-  temp.push_back(minVal);
-  temp.push_back(maxVal);
-  normalizingData.push_back(temp);
-  // Normalize the values in the specified column
-  for (auto &featureVector : data) {
-    double value = featureVector[col];
-    double normalizedValue = (value - minVal) / (maxVal - minVal);
-    featureVector[col] = normalizedValue;
-  }
-}
+unordered_map<int, bool> Search::greedyForward(int totalFeatures)
+{
+  srand(time(NULL));
+  cout << "--------------------------" << endl; // added for clarity
+  cout << "Greedy Forward Search:" << endl;
+  cout << "--------------------------" << endl; // added for clarity
+  unordered_map<int, bool> currSubset;
+  unordered_map<int, bool> bestSubset;
+  double bestAcc = evaluate(currSubset);
+  printFeatures(currSubset, bestAcc);
+  cout << endl;
+  for (unsigned i = 0; i < totalFeatures; ++i)
+  {
+    double currBestAcc = 0.0;
+    int bestFeature = -1;
+    for (unsigned j = 0; j < totalFeatures; ++j)
+    {
+      // if the feature is not already in the current subset:
+      if (currSubset.find(j) == currSubset.end())
+      {
+        unordered_map<int, bool> tempset = currSubset;
+        tempset[j] = true; // explore next feature
 
-int Classifier::nn(vector<double> &features) {
-  int nnclass = -1; // might need to be float?
-  double minDist = numeric_limits<double>::max();
-  // for nearest neighbor, go through every data point (row)
-  // see which has closest features (euclid)
-
-  for (int row = 0; row < data.size(); ++row) {
-    // if the next neighbor checked has a smaller dist, make that
-    // the new nnclass
-    double temp = distance(features, row);
-    if (temp < minDist) {
-      minDist = temp;
-      nnclass = static_cast<int>(data[row][0]); // 1st col is class
+        double a = evaluate(tempset);
+        printFeatures(tempset, a);
+        if (a > currBestAcc)
+        { // if feature yields more accuracy than last, set as best feat
+          currBestAcc = a;
+          bestFeature = j;
+        }
+      }
+    }
+    if (bestFeature != -1)
+    { // if we found a best feature to add
+      currSubset[bestFeature] = true;
+      if (currBestAcc > bestAcc)
+      { // check if this is an overall better subset
+        bestAcc = currBestAcc;
+        bestSubset = currSubset;
+        bestAccuracy = bestAcc;
+      }
+      cout << endl
+           << "Best subset: ";
+      printFeatures(currSubset, currBestAcc);
+      cout << endl;
     }
   }
-  return nnclass;
-}
 
-// Normalize the features
-void Classifier::normalizeFeatures(vector<double> &features) {
-  for (int i = 0; i < numFeatures; i++) {
-    double value = features[i];
-    double normalizedValue = (value - normalizingData[i][0]) /
-                             (normalizingData[i][1] - normalizingData[i][0]);
-    features[i] = normalizedValue;
-  }
-}
-
-// Function to print a 2D vector with the index as the id
-void Classifier::print() {
-  // Print the table header
-  cout << "--------------------------------------------------------------------"
-          "-------------------------"
-       << endl;
-  printf("ID\tClass\t");
-  for (int i = 1; i <= numFeatures; i++) {
-    printf("F_%d\t", i);
-  }
-  printf("\n");
-  cout << "--------------------------------------------------------------------"
-          "-------------------------"
-       << endl;
-
-  // Print the table rows
-  for (int i = 0; i < data.size(); i++) {
-    printf("%d\t", i);
-    for (int j = 0; j < data[i].size(); j++) {
-      printf("%.2f\t", data[i][j]);
-    }
-    cout << endl;
-  }
-}
-
-// Get the number of features
-int Classifier::getNumFeatures() { return numFeatures; }
-
-// Get the number of data points
-int Classifier::getNumDataPoints() { return data.size(); }
-
-const vector<vector<double>>& Classifier::getData() const {
-    return data;
+  return bestSubset;
 }
