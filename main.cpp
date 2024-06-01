0ded4b3713426f0d23702ebdf4ab50a26f5909e5
Elena <elenamardi03@gmail.com>
2024-06-01 16:04:28 -0700

    added usage error for main

--- main.cpp
+++ main.cpp
@@ -14,6 +14,10 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
+    if (argc < 2){
+        cout << "Usage: ./executable filepath" << endl;
+        return 1; 
+    }
 
     Search search;
     string dataLoc = argv[1];
